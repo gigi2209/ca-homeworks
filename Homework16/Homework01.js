62ec99d5e16f3fdf1988a068c0957ae466413bd4
gigi2209 <gligorsimonov@icloud.com>
2020-04-28 21:50:23 +0200

    Homework16 uploaded

---
+++ Homework16/Homework01.js
@@ -0,0 +1,48 @@
+/*
+Homework01
+1) Create html page with bootstrap select and button HTML elements as on video. Homework1601.mov
+2) When you select class option in select element the button class and text should change
+3) Use bootstrap 4 classes: btn-primary, btn-secondary, btn-success, btn-danger, btn-warning, btn-info, btn-dark
+4) When option "Select class" is selected you should disable the button
+More read here : https://getbootstrap.com/docs/4.4/components/buttons/#disabled-state
+
+EXTRA!
+If you done with this task, try to make the button to change the color and text when you press on it!
+Colors(classes) shall change in order as in point (3)
+
+HINTS!! ARE on Bottom of this file!
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* HINTS!
+ Use "change" event to track changes on select element
+ Use this.value to get the value of selected option
+ Use button.setAttribute('disabled', "") to disable the button and button.removeAttribute('disabled') to enable button again
+*/
---
+++ Homework16/Homework02.js
@@ -0,0 +1,54 @@
+/*
+Homework16 02 - Advanced ! On Djina request :)
+This homework may require some knowledge that we havent covered yet with our lessons.
+Use w3schools.com website for refferences.
+
+1) Create functionality as on video
+2) There are 16 squares when you click on each it change background color randomly
+
+HINTS are at the bottom!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+HINTS!
+1) You should use CSS rgb() to randomly change the background colors
+2) To generate random number in given range use:  Math.floor(Math.random() * maximumNumber);
+Where maximumNumber is variable you pass as maximum range number.
+For example :
+Math.floor(Math.random() * 255) will give you random number from 0 to 255
+3) Use element.onclick = function() { ... } to assign event toa each square
+4) Use loops to generate squares
+5) Use loops to attach function to onlclick
+*/
